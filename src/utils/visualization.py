6e9baf565dee738cbdca9177f079e1ac9188e737
Pavlo Velychko <112926321+Antibiot1c@users.noreply.github.com>
2024-06-02 18:03:55 +0300

    Create visualization.py

---
+++ src/utils/visualization.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plot_sample_images(train_images, train_labels, class_names):
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i][0]])
+    plt.show()
